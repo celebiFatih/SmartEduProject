6e6da98ca5a473c6bcaf7cc1cd7c4ba1259f0faf
fatih celebi <fatihcelebi13@gmail.com>
2022-05-17 22:25:35 +0300

    working 02

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  res.status(200).send('INDEX SAYFASI');
+  res.status(200).render('index',{
+      page_name: "index" //template engine page_name isimli değişken gonderiyoruz. sayfa aktif olanı gosterilmesinde kullanamk için
+  });
+});
+app.get('/about', (req, res) => {
+  res.status(200).render('about',{
+    page_name: "about"
+});
 });
 
 const port = 3000;
