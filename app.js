d55f65ae00d6a8970a7661920f32b207cb7f190d
fatih celebi <fatihcelebi13@gmail.com>
2022-05-18 17:53:27 +0300

    working 05

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 //ROUTES
 app.use('/', pageRoute); // '/' isteği geldiği zaman pageRoute' u kullan. tüm gelen istekler pageRoute'a yönlendiriliyor
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -4,16 +4,14 @@ const Course = require('../models/Course'); // kurs olusturmak için model dosya
 // Create Course
 exports.createCourse = async (req, res) => {
   //kurs olusturma sayfası henuz hazır olmadığı için burada once onun simulasyonunu yapacağız
-  const course = await Course.create(req.body); // req.body kursu doldurmak için gerekli olan formdan gelecek bilgiler
-
-  // aldığımız cevap. henuz template'e render etmiyoruz. onun yerine json dosyası olarak yazdıyoruz
-  /* bu işlemi su soyle dusunmeliyiz: biz backend'de calısıypruz ve frontend henuz hazır değil. ancak backendde yaparkende
-    bizde bir şekilde yeni bir kurs olusturma isteği gelmiş giib davranmamız lazım. bunun için postman yazılımından faydalanacağız.*/
+  
+  // aldığımız cevap. henuz template'e render etmiyoruz. onun yerine json dosyası olarak yazdıyoruz  
   try {
+    const course = await Course.create(req.body); // req.body kursu doldurmak için gerekli olan formdan gelecek bilgiler
     res.status(201).json({
       status: 'success',
       course,
-    }); // yeni olusturm için 201 kodu
+    }); // yeni olusturma için 201 kodu
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -21,3 +19,6 @@ exports.createCourse = async (req, res) => {
     });
   }
 };
+/* bu işlemi soyle dusunmeliyiz: biz backend'de calısıypruz ve frontend henuz hazır değil. ancak backendde yaparkende
+    bizde bir şekilde yeni bir kurs olusturma isteği gelmiş giib davranmamız lazım. bunun için postman yazılımından faydalanacağız.
+*/
